2a5fefb38dfb7733ebf5cc42a1edbd06ad1d5e5b
Tyler Kazar <tk322@njit.edu>
2020-02-25 12:53:22 -0500

    major debugging with files. now sends error to rabbit using errorLogging class.

---
+++ testError.php
@@ -0,0 +1,13 @@
+#!/usr/bin/php
+<?php
+
+require_once('ErrorLogger.php.inc');
+
+$rabbitConfig = 'rabbit.ini';
+$log = new errorLogger('dbtest',$rabbitConfig);
+
+
+$log->logError("Test","DEBUG","This is a test Error");
+
+
+
