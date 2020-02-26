6461860ff3d95f311f3d285c0caa15db8d6175ef
Tyler Kazar <tk322@njit.edu>
2020-02-25 20:29:19 -0500

    Created updated version of ErrorLogger.php.in called Logger.php.inc. This includes a new debug method to streamline those types of logging messages, and broadens the scope of the overall functionality.

--- testError.php
+++ testError.php
@@ -1,13 +1,14 @@
 #!/usr/bin/php
 <?php
 
-require_once('ErrorLogger.php.inc');
+require_once('Logger.php.inc');
 
 $rabbitConfig = 'rabbit.ini';
-$log = new errorLogger('dbtest',$rabbitConfig);
+$log = new Logger('dbtest',$rabbitConfig);
 
 
 $log->logError("Test","DEBUG","This is a test Error");
+$log->logDebug("test","this is a test debug");
 
 ?>
 
